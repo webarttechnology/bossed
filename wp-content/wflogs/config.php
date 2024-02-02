139579ac65ad78da9d45751610558d774d2815cf
pushanwebart <pushan.chakraborty@webart.technology>
2024-02-02 16:37:26 +0530

    final

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1707476130;s:7:"authKey";s:64:"3|o1E).D(Kf7Y<QK)E[uA.o4s:K nUS(EJVz9WO<CO9*mwl8;M)h5;%7~RTHu1Y2";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1030;}
